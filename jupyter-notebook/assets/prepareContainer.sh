8a0cb176bdb7ab4882e58ae10d6af6e467126a18
Lucas Jellema <lucas.jellema@amis.nl>
2019-02-04 15:54:18 +0100

    Adding notebook to preparation of container

--- jupyter-notebook/assets/prepareContainer.sh
+++ jupyter-notebook/assets/prepareContainer.sh
@@ -10,4 +10,10 @@ jupyter labextension install @jupyterlab/plotly-extension
 # perhaps clone a github repo into /home/jovyan/work
 cd ~/work
 git clone http://bitbucket.org/hrojas/learn-pandas
-
+# round about way to get a single file into the running container
+mkdir temp
+cd temp
+git clone https://github.com/lucasjellema/katacoda-scenarios
+cp katacoda-scenarios/jupyter-notebook/assets/introPythonForDataAnalysis.ipynb ~/work
+cd ~/work
+rm -R temp 
