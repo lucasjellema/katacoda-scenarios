7353b85d7373f1a343d91c33053653e74a1bcd58
Lucas Jellema <lucas.jellema@amis.nl>
2019-02-04 13:35:18 +0100

    Adding Jupyter Notebook pythonForDataAnalysis as standard asset

--- jupyter-notebook/assets/runPrep.sh
+++ jupyter-notebook/assets/runPrep.sh
@@ -1,5 +1,6 @@
 # copy the script into the container; note: the resulting file is owned by root and it not executable
 docker cp prepareContainer.sh jupyter:/home/jovyan/prepareContainerRoot.sh
+docker cp pythonForDataAnalysis.ipynb jupyter:/home/jovyan/work/pythonForDataAnalysis.ipynb
 # copy the script to make it accessible and executable 
 docker exec -d jupyter bash -c 'cp ~/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh'
 # execute the script inside the container - to install a number of packages
