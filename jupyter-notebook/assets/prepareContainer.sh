35fba3cabb4a7be9083b5e7e5b63005dd63c4988
Lucas Jellema <lucas.jellema@amis.nl>
2019-02-06 06:47:50 +0100

    Adding latest SciPy docker image; adding ipywdigets

--- jupyter-notebook/assets/prepareContainer.sh
+++ jupyter-notebook/assets/prepareContainer.sh
@@ -1,11 +1,17 @@
 pip install --upgrade pip
+pip install --upgrade pandas
 git clone https://github.com/kavgan/word_cloud 
 cd word_cloud && pip install -e . 
+cp  ~/word_cloud/*clouds.ipynb  ~/work
 pip install plotly 
 pip install matplotlib_venn 
 pip install cufflinks 
 pip install gender-guesser
 pip install -U nltk
+pip install ipywidgets
+pip install pyarrow
+
+jupyter nbextension enable --py widgetsnbextension
 jupyter labextension install @jupyterlab/plotly-extension
 # perhaps clone a github repo into /home/jovyan/work
 cd ~/work
@@ -15,5 +21,9 @@ mkdir temp
 cd temp
 git clone https://github.com/lucasjellema/katacoda-scenarios
 cp katacoda-scenarios/jupyter-notebook/assets/introPythonForDataAnalysis.ipynb ~/work
+# adding notebook to play with ipywidgets
+git clone https://github.com/WillKoehrsen/Data-Analysis
+cp Data-Analysis/widgets ~/work
+
 cd ~/work
 rm -Rf temp 
