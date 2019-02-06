7b0f6f91fe9f1d7a6124e90ace8f79c8612b9b7f
Lucas Jellema <lucas.jellema@amis.nl>
2019-02-06 07:09:58 +0100

    Correcting packages installation

--- jupyter-notebook/assets/prepareContainer.sh
+++ jupyter-notebook/assets/prepareContainer.sh
@@ -1,8 +1,7 @@
 pip install --upgrade pip
-pip install --upgrade pandas
 git clone https://github.com/kavgan/word_cloud 
 cd word_cloud && pip install -e . 
-cp  ~/word_cloud/*clouds.ipynb  ~/work
+cp  ~/word_cloud/*clouds.ipynb  ~/work/Example_word_clouds.ipynb
 pip install plotly 
 pip install matplotlib_venn 
 pip install cufflinks 
@@ -10,20 +9,22 @@ pip install gender-guesser
 pip install -U nltk
 pip install ipywidgets
 pip install pyarrow
+pip install --upgrade pandas
+
 
 jupyter nbextension enable --py widgetsnbextension
-jupyter labextension install @jupyterlab/plotly-extension
 # perhaps clone a github repo into /home/jovyan/work
 cd ~/work
 git clone http://bitbucket.org/hrojas/learn-pandas
-# round about way to get a single file into the running container
+git clone https://github.com/WillKoehrsen/Data-Analysis
+cp -R Data-Analysis/widgets ~/work
+# next follows a round about way to get a single file into the running container
 mkdir temp
 cd temp
 git clone https://github.com/lucasjellema/katacoda-scenarios
 cp katacoda-scenarios/jupyter-notebook/assets/introPythonForDataAnalysis.ipynb ~/work
 # adding notebook to play with ipywidgets
-git clone https://github.com/WillKoehrsen/Data-Analysis
-cp Data-Analysis/widgets ~/work
+
 
 cd ~/work
 rm -Rf temp 
