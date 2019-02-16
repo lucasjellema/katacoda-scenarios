030c9f15c565c708963207d37f4457a442724826
Lucas Jellema <lucas.jellema@amis.nl>
2019-02-16 09:02:19 +0100

    Adding HelloWorld Notebook

--- jupyter-notebook/assets/prepareContainer.sh
+++ jupyter-notebook/assets/prepareContainer.sh
@@ -13,10 +13,6 @@ pip install pyarrow
 # word_cloud has a fixed dependency on an older version of pandas - and when installed will overwrite the current one that comes in the Docker image
 # here I am force installing the latest pandas - required by the widgets notebook; word_cloud will still work
 pip install --upgrade pandas
-# apparently the base image already contain ipywidgets
-#jupyter nbextension enable --py widgetsnbextension
-
-# perhaps clone a github repo into /home/jovyan/work
 cd ~/work
 git clone http://bitbucket.org/hrojas/learn-pandas
 # adding notebook to play with ipywidgets
@@ -26,11 +22,13 @@ mkdir temp
 cd temp
 # next follows a round about way to get a single file into the running container
 git clone https://github.com/lucasjellema/katacoda-scenarios
-mv katacoda-scenarios/jupyter-notebook/assets/introPythonForDataAnalysis.ipynb ~/work
+cp katacoda-scenarios/jupyter-notebook/assets/introPythonForDataAnalysis.ipynb ~/work
 # clone the data wrangling workshop github repo
 git clone https://github.com/lucasjellema/DataAnalytics--IntroductionDataWrangling-JupyterNotebooks
 # then move folder CaseOfOracleOpenWorld2018 under ~/work
 mv DataAnalytics--IntroductionDataWrangling-JupyterNotebooks/CaseOfOracleOpenWorld2018 ~/work
+# and move folder HelloWorldNotebook under ~/work
+mv DataAnalytics--IntroductionDataWrangling-JupyterNotebooks/HelloWorldNotebook ~/work
 
 cd ~/work
 rm -Rf temp 
--- jupyter-notebook/assets/runPrep.sh
+++ jupyter-notebook/assets/runPrep.sh
@@ -1,14 +1,13 @@
 # copy the script into the container; note: the resulting file is owned by root and it not executable
-docker cp prepareContainer.sh jupyter:/home/jovyan/prepareContainerRoot.sh
+docker cp prepareContainer.sh jupyter:/tmp/prepareContainerRoot.sh
 # copy the script to make it accessible and executable 
-docker exec -d jupyter bash -c 'cp ~/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh'
+docker exec -it jupyter bash -c 'cp /tmp/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh'
 # execute the script inside the container - to install a number of packages
-docker exec -it jupyter sh /home/jovyan/prepareContainer.sh
-# restart the docker container
+docker exec -it jupyter bash -c './prepareContainer.sh'
+# # restart the docker container
 docker restart jupyter
 echo 'restarted Jupyter container'
 # perhaps clone a GitHub repo inside the Jupyter container
-# docker exec -it jupyter git clone https://github.com/lucasjellema/Oracle-OpenWorld-CodeOne-2018-SessionCatalog
+#docker exec -it jupyter git clone https://github.com/lucasjellema/DataAnalytics--IntroductionDataWrangling-JupyterNotebooks
 # and more stuff?
-# docker logs jupyter
-
+docker logs jupyter
