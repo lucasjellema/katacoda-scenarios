6612987cb5429726fa925f162b24abe0cb5ad1c0
Lucas Jellema <lucas.jellema@amis.nl>
2020-01-07 07:13:47 +0100

    Installing OCI CLI container and Fn CLI

--- oci-functions-1/background.sh
+++ oci-functions-1/background.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 touch /root/createdFromBackgroundScript
+# start Docker Container with OCI 
+mkdir ~/.oci
+docker run --rm --mount type=bind,source=$HOME/.oci,target=/root/.oci -it stephenpearson/oci-cli:latest 
+#setup config
+#
+# add this line to ~/.profile
+# oci() { docker run --rm --mount type=bind,source=$HOME/.oci,target=/root/.oci stephenpearson/oci-cli:latest "$@"; }
+echo 'oci() { docker run --rm --mount type=bind,source=$HOME/.oci,target=/root/.oci stephenpearson/oci-cli:latest "$@"; }' >> ~/.profile
+# reload ~/.profile
+. ~/.profile
+
+# install Project Fn CLI
+curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh
