5f23ab9166a5ce5806c05ac8dbf20067b71ddea1
Lucas Jellema <lucas.jellema@amis.nl>
2020-01-27 17:41:45 +0100

    .

--- jq-intro/foreground.sh
+++ jq-intro/foreground.sh
@@ -1,13 +1,3 @@
-echo "Installing OCI CLI - in the background"
-
-export OCI_CLI_SUPPRESS_FILE_PERMISSIONS_WARNING=True
-
-# add this line to ~/.profile - to make oci a recognized shell command
-echo 'oci() { /root/bin/oci "$@"; }' >> ~/.profile
-# reload ~/.profile
-. /root/.profile
-# now oci is recognized as a command
-
-echo "Wait for the file /root/readyWithBackground to appear"
+echo "Get going...."
 
 
